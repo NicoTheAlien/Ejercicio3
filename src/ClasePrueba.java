5ec638f30978f7cdb5f93f8cebfd97d99fc53bbf
Nico <nfiorito@nigul.cide.es>
2019-11-11 13:37:01 +0100

    modificacion llamada

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -1,11 +1,7 @@
 
 public class ClasePrueba {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		printHola();
-	}
-	public static void printHola() {
+	public void printHola() {
 		System.out.println("Hola món");
 	}
 
---
+++ src/Llamada.java
@@ -0,0 +1,11 @@
+
+public class Llamada {
+
+	public static void main(String[] args) {
+		ClasePrueba Llamada=new ClasePrueba();
+		
+		Llamada.printHola();
+
+	}
+
+}
