980f6bf1aea3462389a37d150268a837f32ce36c
Nico <nfiorito@nigul.cide.es>
2019-11-11 02:39:00 +0100

    modificacion printHola

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -3,6 +3,9 @@ public class ClasePrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		printHola();
+	}
+	public static void printHola() {
 		System.out.println("Hola món");
 	}
 
