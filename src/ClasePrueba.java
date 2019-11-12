ae763ebd5f23e9511159c9a204369be06a7ab460
Nico <nfiorito@nigul.cide.es>
2019-11-12 14:05:25 +0100

    modficacion codigo suma

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -4,5 +4,8 @@ public class ClasePrueba {
 	public void printHola() {
 		System.out.println("Hola món");
 	}
-
+	public static void main(String[] args) {
+		System.out.println("1+1 es 2");
+	}
 }
+	
