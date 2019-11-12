019562f4da513312f1b9281463f4e4d30478c492
Nico <nfiorito@nigul.cide.es>
2019-11-12 14:14:39 +0100

    primer commit test

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -4,8 +4,6 @@ public class ClasePrueba {
 	public void printHola() {
 		System.out.println("Hola món");
 	}
-	public static void main(String[] args) {
-		System.out.println("1+1 es 2");
-	}
+
 }
 	
